2ed7113d343f387ed252e3e5f2fe379b3986775a
MAKSIM <max_air@bk.ru>
2025-03-17 15:22:33 +0300

    feat: left 7 errors in app.ts and appView.Ts

--- src/types.ts
+++ src/types.ts
@@ -13,5 +13,25 @@ export interface IGetResp {
 
 export interface IDataNews {
     sources?: String[],
-    articles?: String[]
+    articles?: IArticlesData[]
 };
+
+
+export interface IArticlesData {
+  source: {
+    id: string,
+    name: string
+  },
+  author: string,
+  title: string,
+  description: string,
+  url: string,
+  urlToImage: string,
+  publishedAt: string,
+  content: string
+}
+
+export interface ISourcesData {
+  id: string,
+name: string,description: "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.",url: "https://abcnews.go.com",category: "general",language: "en",country: "us"
+}
