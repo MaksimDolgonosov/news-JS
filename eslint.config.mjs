d25a2d91c7c2f2571f4717a1b32b9531a2d71cfd
MAKSIM <max_air@bk.ru>
2025-03-18 19:50:03 +0300

    feat: allowdefaultProject

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+//import tslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
+            parser: tseslint.parser,
+            parserOptions: {
+                project: "./tsconfig.json",
+                tsconfigRootDir: import.meta.dirname,
+                createDefaultProgram: true,
+                projectService: {
+                  allowDefaultProject: ["eslint.config.mjs", "./build/*.js", "./src/*.ts"],
+                  defaultProject: "tsconfig.json",
+              },
+                // loadTypeScriptPlugins: !!process.env.VSCODE_PID,
+                sourceType: "module",
+            },
+        },
+    rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-require-imports": "off"
+   },
   },
-  rules: {
-    "@typescript-eslint/no-explicit-any": "error"
-  }
+
 );
